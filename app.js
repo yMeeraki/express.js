b53be5ceb40d249665f15d6877f0ef5016fbe1b7
Yukti Gupta <yuktigupta95@gmail.com>
2026-02-02 20:15:01 +0530

    Make 404 Page in Node.js

--- app.js
+++ app.js
@@ -2,19 +2,24 @@ import express from "express";
 import { resolve } from "path";
 
 const app = express();
+const absolutePath = resolve(`view`); // To get absolute path
 
 app.get("/", (req, res) => {
   // console.log(__dirname) // Won't work as we are using module
-  // console.log(resolve(`view/home.html`)); // To get absolute path
-  res.sendFile(resolve(`view/home.html`));
+
+  res.sendFile(`${absolutePath}/home.html`);
 });
 
-app.get("/login",(req,res)=>{
-  res.sendFile(resolve(`view/login.html`));
-})
+app.get("/login", (req, res) => {
+  res.sendFile(resolve(`${absolutePath}/login.html`));
+});
 
-app.get("/submit",(req,res)=>{
-  res.sendFile(resolve(`view/submit.html`));
-})
+app.get("/submit", (req, res) => {
+  res.sendFile(resolve(`${absolutePath}/submit.html`));
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile(resolve(`${absolutePath}/404.html`));
+});
 
 app.listen(3200);
