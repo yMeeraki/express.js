546de950d224ca13988a3885fbbb93ba088b47a5
Yukti Gupta <yuktigupta95@gmail.com>
2026-02-02 18:46:15 +0530

    How express js works explained

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require("express");
-const app = express()
+const app = express();
 
-app.get("/",(req,res)=>{
-res.send("<h1>Hello World</h1>")  
-})
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World</h1>");
+});
 
-app.listen(3200)
+app.get("/about", (req, res) => {
+  res.send(`<p>Hello World: About</p> 
+  <input type="text">
+  <h1>WOW</h1>
+  `);
+});
+
+app.listen(3200);
